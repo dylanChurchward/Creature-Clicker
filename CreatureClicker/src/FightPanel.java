5ec50e39d73adf7adf4daef2ad45fe9847b5698a
Dylan <dylanchurchward710@gmail.com>
2021-09-28 20:48:15 -0700

    Having issues committing another project, testing to see if this one will work.

--- CreatureClicker/src/FightPanel.java
+++ CreatureClicker/src/FightPanel.java
@@ -20,7 +20,7 @@ import javax.swing.Timer;
 import Creatures.CreatureList;
 import Creatures.CreatureNode;
 
-public class FightPanel extends JPanel implements MouseListener{
+public class FightPanel extends JPanel implements MouseListener {
 
 	// JPanels like to be serialized
 	private static final long serialVersionUID = 1L;
